8f7fac0bfddff8fee35931fefc6309cd6697c20d
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-06-17 10:07:08 +0530

    updated

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react'
 import { QuizContext } from '../Helpers/Contexts';
 import { Questions } from "../Helpers/QuestionBank";
 
+
 function Quiz() {
     const { score, setScore, setGameState } = useContext(QuizContext);
 
@@ -29,13 +30,25 @@ function Quiz() {
 
     return (
         <div className="Quiz">
-            {/* <h1 id="countdown"></h1> */}
             <h1>{Questions[currQuestion].prompt}</h1>
             <div className="options">
-                <button onClick={() => setOptionChosen("A")}>{Questions[currQuestion].optionA}</button>
-                <button onClick={() => setOptionChosen("B")}>{Questions[currQuestion].optionB}</button>
-                <button onClick={() => setOptionChosen("C")}>{Questions[currQuestion].optionC}</button>
-                <button onClick={() => setOptionChosen("D")}>{Questions[currQuestion].optionD}</button>
+                <button onClick={() => { 
+                    setOptionChosen("A");
+                    }}>
+                    {Questions[currQuestion].optionA}
+                </button>
+                <button onClick={() => {
+                    setOptionChosen("B");
+                    }}>{Questions[currQuestion].optionB}
+                    </button>
+                <button onClick={() => {
+                    setOptionChosen("C");
+                    }}>{Questions[currQuestion].optionC}
+                    </button>
+                <button onClick={() => {
+                    setOptionChosen("D");
+                    }}>{Questions[currQuestion].optionD}
+                    </button>
             </div>
 
             <div className="nextprev">
